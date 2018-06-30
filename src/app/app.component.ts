0d88b2a7f9f4d4fbc9abf033a276a55f65007059
11600912 <m.divesh98@gmail.com>
2018-06-30 16:08:36 +0530

    fist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl:`./app.component.html`,
+  styleUrls: [`./app.component.css`]
+
 })
 export class AppComponent {
-  title = 'app';
+  public title={
+    textAlign:"center",
+    color:"black",
+    fontFamily:"verdana",
+    margin:"5px",
+    padding:"3px"
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms';
+import { RegisterationComponent } from './registeration/registeration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
